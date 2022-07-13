87677436afe605fe5a4d40630f431fddca8a2d6c
Capybara <yskiwolf01@rambler.ru>
2022-07-13 22:58:30 +0300

    init commit

---
+++ src/components/head.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './head.css'
+ const Head = (props:any)=>{
+    return(
+ <div className="head">
+    <div className="head-flex">
+         <div className="head-f">
+                 <div>
+                    <h1 className="head_z">Мы - 10 ПРОГ</h1>
+                 </div>
+
+
+
+                <div>
+                    <p className="head_p">А это наша стенгазета</p>
+                 </div>
+        </div>  
+        
+    </div>
+        
+</div>
+    )
+}
+export default Head;
