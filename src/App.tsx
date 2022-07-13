4b1012b38ad712bc19e771d035dfb627c775de37
Capybara <yskiwolf01@rambler.ru>
2022-07-14 00:25:29 +0300

    Rereacting

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <Head/>
       <Text/>
       <Sw/>
-     <Petr/>
+     т
     </div>
    
   )
--- src/components/P.tsx
+++ src/components/P.tsx
@@ -17,12 +17,13 @@ const Petr = ()=>{
 </div>
 <div>
 <p >
-<img className = "petya" src="petya.jpg"> <a className="anek">"У берегов Голландии перевернулся танкер. Перевернулся и поплыл на спинке."</a> </img>
+{/* <img className = "petya" src="petya.jpg"> <a className="anek">"У берегов Голландии перевернулся танкер. Перевернулся и поплыл на спинке."</a> </img> */}
 </p>
 </div>
 <div>
 <p>
-   <a className="anek">"Идёт мужик,видит – анекдот лежит, перевернул, а там:wɐɯ ɐ ‘vʎнdǝʚǝdǝu 'ɯижǝv ɯоɓʞǝнɐ – ɯиɓиʚ‘ʞижʎw ɯǝ̤ɓи</a>  <img src="petya_mirror.jpg" className="petya"> </img> 
+   <a className="anek">"Идёт мужик,видит – анекдот лежит, перевернул, а там:wɐɯ ɐ ‘vʎнdǝʚǝdǝu 'ɯижǝv ɯоɓʞǝнɐ – ɯиɓиʚ‘ʞижʎw ɯǝ̤ɓи</a>  
+   {/* <img src="petya_mirror.jpg" className="petya"> </img>  */}
     </p>
 </div>
 
